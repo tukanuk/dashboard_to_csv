f1033773a0e353dfbc5e23e3719a8ebaa2187e59
Ben Davidson <ben.davidson@dynatrace.com>
2022-06-09 08:58:57 -0400

    Setup of project

---
+++ settings.py
@@ -0,0 +1,19 @@
+""" Stores settings using python-dotenv. This file can be included in version
+control and contains references to environment variables but checks
+if the variable is present in a .env file (which is not included in
+version control) and uses those values first
+
+Every variable should be included here even it is not present in Environment
+variables.
+
+example
+
+API_TOKEN = os.getenv(".ENV_API_TOKEN")
+
+"""
+
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
